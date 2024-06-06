6e8a0cd1efa02d373a42a9ceb430fa14dc036e8e
Egor Poderiagin <egor@crazyrussian.pro>
2024-06-06 23:32:00 +0700

    feat: add models to admin panel

--- backend/friend_request/admin.py
+++ backend/friend_request/admin.py
@@ -1,1 +1,17 @@
-# Register your models here.
+from django.contrib import admin
+from .models import FriendRequest
+
+
+@admin.register(FriendRequest)
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "from_user",
+        "to_user",
+        "created_at",
+        "accepted_at",
+        "rejected_at",
+    ]
+    search_fields = ["from_user__email", "to_user__email"]
+    list_filter = ["accepted_at", "rejected_at"]
+    ordering = ["id"]
--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "email", "first_name", "last_name"]
+    search_fields = ["email", "first_name", "last_name"]
+    list_filter = ["is_active", "is_staff", "is_superuser"]
+    ordering = ["id"]
